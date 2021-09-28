a90a3d9ee1316a21a6671dfe76cfd7df9d16f130
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2021-09-28 22:26:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,7 @@ terraform {
   }
 }
 
-resource "null_resource" "mynull" {
-  count = 3
 
-  provisioner "local-exec" {
-    command = "echo ${count.index}"
-  }
-
-}
 
 data "extip" "external_ip" {
 }
